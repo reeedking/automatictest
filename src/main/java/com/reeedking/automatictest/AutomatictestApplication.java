85dc6e09cff5b352a11412626247ab4b911dd908
reeedking <121719158@qq.com>
2021-12-14 20:19:47 +0800

    Initial commit

---
+++ src/main/java/com/reeedking/automatictest/AutomatictestApplication.java
@@ -0,0 +1,13 @@
+package com.reeedking.automatictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatictestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomatictestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reeedking/automatictest/AutomatictestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reeedking.automatictest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatictestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
